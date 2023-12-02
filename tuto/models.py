6ad53431a38eb2e5f2258308a7794b26f98d36a8
ahmet40 <40ahmetbaba@gmail.com>
2023-12-02 10:43:37 +0100

    exploit : ajout de la redirection vers les page une fois que l'on c'est connecter ou que l'on à crée le compte

--- tuto/models.py
+++ tuto/models.py
@@ -1,47 +1,65 @@
-import yaml, os.path
-from .app import db
+import yaml
+import os.path
+import sys
 from datetime import datetime
+from .app import db
 from .app import login_manager
+
+
 ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), './')
 sys.path.append(os.path.join(ROOT, 'modele/bd/'))
 from connexion import cnx
 from spectateur_bd import *
+from concert_bd import *
 
 
-def connecter_spectateur(string : username, string : passowrd)-> bool:
+def connecter_spectateur(username: str, password: str) -> bool:
     """Cette fonction va nous permettre de connecter le spectateur à l'application
 
     Args:
-        string (username): le pseudo ou le mail entrer par le spectateur
-        string (passowrd): le mot de apsse du specatateur 
+        username (str): le pseudo ou le mail entré par le spectateur
+        password (str): le mot de passe du spectateur
 
     Returns:
-        bool: ranvoie la valeur true si le spectateur existe dans la base de donnée et false sinon
+        bool: renvoie la valeur True si le spectateur existe dans la base de données et False sinon
     """
-    
     user = Spectateur_bd(cnx)
-    liste_spec=user.get_all_spectateurs()
-    if liste_spec != [] and liste_spec != None:
+    liste_spec = user.get_all_spectateurs()
+    if liste_spec:
         for spec in liste_spec:
-            if (username==spec.get_pseudo() or username==spec.get_email())and password==spec.get_mdp():
+            if (username == spec.get_pseudo() or username == spec.get_email()) and password == spec.get_mdp():
                 return True
     return False
 
 
-def creation_compte(string : username, string : email, string : password) -> bool:
-    """Cette fonction va nous permettre d'inscrire un spectateur si il n'a pas de compte
+def creation_compte(username: str, email: str, password: str) -> bool:
+    """Cette fonction va nous permettre d'inscrire un spectateur s'il n'a pas de compte
 
     Args:
-        string (username): le nom entrer par l'utilisateur
-        string (email): le mail entrer par l'utilisateur
-        string (password): le mot de passe entrer par l'utilisateur
+        username (str): le nom entré par l'utilisateur
+        email (str): le mail entré par l'utilisateur
+        password (str): le mot de passe entré par l'utilisateur
     """
     user = Spectateur_bd(cnx)
-    liste_spec=user.get_all_spectateurs()
-    if liste_spec != [] and liste_spec != None:
+    liste_spec = user.get_all_spectateurs()
+    if liste_spec:
         for spec in liste_spec:
-            if username==spec.get_pseudo() and email==spec.get_email() and password==spec.get_mdp():
-                print("vous etes deja inscrit")
+            if email==spec.get_email():
+                print("Vous avez déjà un compte associer à ce mail")
                 return False
-    user.inserer_spectateurs(user.get_prochain_id_spectateur(),username,email,password)
+            elif username == spec.get_pseudo() and email == spec.get_email() and password == spec.get_mdp():
+                print("Vous êtes déjà inscrit.")
+                return False
+    user.inserer_spectateurs(user.get_prochain_id_spectateur(), username, email, password)
     return True
+
+
+
+def liste_concert()->list:
+    """cette methode va nous permettre d'obtenir tous les concert du festivale
+
+    Returns:
+        list: la liste des concerts.
+    """
+    user = Concert_bd(cnx)
+    return user.get_all_concert()
--- tuto/view.py
+++ tuto/view.py
@@ -9,6 +9,7 @@ from wtforms import PasswordField
 from flask import request, redirect, url_for
 from wtforms import FloatField
 from flask import flash
+import models
 from .app import app, db
 import sqlalchemy
 import os
@@ -21,12 +22,61 @@ def home():
 
 @app.route("/login_spec")
 def login_spec():
+    """Cette methode va nous permettre de nous diriger vers la page 
+        login pour les spectateurs.
+    Returns:
+        reder_template:direction vers la page
+    """
     return render_template("login_spec.html")
 
 @app.route("/login_admin")
 def login_admin():
+    """Cette methode va nous permettre de nous diriger vers la page 
+        login pour les admin
+    Returns:
+        reder_template:direction vers la page
+    """
     return render_template("login_admin.html")
 
 @app.route("/create_account")
 def create_account():
+    """Cette methode va nous permettre de nous diriger vers la page 
+        creation de compte
+    Returns:
+        reder_template:direction vers la page
+    """
     return render_template("create_account.html")
+
+@app.route("/liste_concerts")
+def liste_concert():
+    """Cette methode va nous permettre de nous diriger vers la page 
+        liste de concerts
+    Returns:
+        reder_template:direction vers la page
+    """
+    return render_template("liste_concerts.html",liste_concert=models.liste_concert())
+
+@app.route("/login_spec",methods=["GET","POST"])
+def creation_compte():
+    """Cette methode va nous permettre de nous rediriger vers la page 
+        login
+    Returns:
+        redirect:redirection vers la page
+    """
+    username=request.form.get("username")
+    email=request.form.get("email")
+    password=request.form.get("password")
+    return redirect(url_for("login_spec"))
+
+@app.route("/liste_concerts",methods=["GET","POST"])
+def se_connecter():
+    """Cette methode va nous permettre de nous rediriger vers la page 
+        liste des concerts
+    Returns:
+        redirect:redirection vers la page
+    """
+    username=request.form.get("username")
+    password=request.form.get("password")
+    if not models.connecter_spectateur(username,password):
+        return redirect(url_for("login_spec"))
+    return redirect(url_for("liste_concerts"))
